894c00bd89628fa0a75b325b0531034938802748
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-12-09 01:18:09 +0530

    code comleted

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, SelectField
+from wtforms_components import TimeField
+from wtforms.validators import DataRequired, URL
 import csv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
+ratings = ["☕️", "💪", "✘", "🔌"]
+
+
+def rate():
+    rating_list = []
+    for n in ratings:
+        icons = []
+        for i in range(1, 6):
+            icons.append(n * i)
+        rating_list.append(icons)
+    return rating_list
+
+
+rating = rate()
+
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
-
+    cafe_location_on_google_maps = StringField('Cafe Location On Google Maps', validators=[DataRequired(), URL()])
+    opening_time = TimeField("Opening Time", validators=[DataRequired()])
+    closing_time = TimeField("Closing Time", validators=[DataRequired()])
+    coffee_rating = SelectField(choices=rating[0])
+    wifi_strength_rating = SelectField(choices=rating[1])
+    power_socket_availability = SelectField(choices=rating[3])
     submit = SubmitField('Submit')
 
 
@@ -22,14 +43,18 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("True")
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
+        with open("cafe-data.csv", mode="a") as data:
+            data.write(f"\n{form.cafe.data},"
+                       f"{form.cafe_location_on_google_maps.data},"
+                       f"{form.opening_time.data},"
+                       f"{form.closing_time.data},"
+                       f"{form.coffee_rating.data},"
+                       f"{form.wifi_strength_rating.data},"
+                       f"{form.power_socket_availability.data}")
     return render_template('add.html', form=form)
 
 
